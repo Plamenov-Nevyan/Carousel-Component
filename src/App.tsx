bfba74fb0cd825d9d7cc82c08113016dfd4b393f
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-01-25 09:17:01 +0200

    Completed implementing basic html/css structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,24 @@ import styles from "./app.module.css"
 import { Slide } from './components/Slide/Slide'
 
 const pictures = [
-  "../public/image1.jpg",
-  "../public/image2.jpg",
-  "../public/image3.jpg"
+  {src : "/image1.jpg", caption : "image 1"},
+  {src : "/image2.jpg", caption : "image 2"},
+  {src : "/image3.jpg", caption : "image 3"},
 ]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hi</h1>
-  // <div className={styles["carousel-container"]}>
-  //        {pictures.map(pic => < Slide />)}
-  // </div>
+  <div className={styles["carousel-container"]}>
+         {pictures.map((pic, index) => <Slide 
+         slidePicture={pic.src} 
+         slideIndex={index + 1} 
+         caption={pic.caption} 
+         totalSlides={pictures.length}
+         />
+         )}
+  </div>
   )
 }
 
--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -1,14 +1,14 @@
 import styles from "./slide.module.css"
 type SlideProps = {
     slidePicture : string,
-    slideIndex : string,
+    slideIndex : number,
     caption : string,
     totalSlides : number,
 }
 
 export function Slide({slidePicture, slideIndex, caption, totalSlides} : SlideProps){
     return (
-        <div className={styles["slide-container"]}>
+        <div className={`${styles["slide-container"]} ${styles.fade}`}>
            <div className={styles["number-caption"]}>{slideIndex}/{totalSlides}</div>
            <img src={slidePicture} className={styles["slide-picture"]}></img>
            <div className={styles["text-caption"]}>{caption}</div>
