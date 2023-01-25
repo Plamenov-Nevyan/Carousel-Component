6a2b880cde015860b35f93a14652ceff342711ba
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-01-25 08:58:38 +0200

    Added Slide component and basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
-import './App.css'
+import styles from "./app.module.css"
+import { Slide } from './components/Slide/Slide'
+
+const pictures = [
+  "../public/image1.jpg",
+  "../public/image2.jpg",
+  "../public/image3.jpg"
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (<h1>Hi</h1>
+  return (
+    <h1>Hi</h1>
+  // <div className={styles["carousel-container"]}>
+  //        {pictures.map(pic => < Slide />)}
+  // </div>
   )
 }
 
